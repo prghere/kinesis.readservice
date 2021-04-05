0e3e486d272c729d6da6ff2afc5375557d7340e0
prabhu <prghere@gmail.com>
2021-04-05 07:24:39 -0400

    first commit

---
+++ src/test/java/com/erdossoftwsre/kinesis/readservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdossoftwsre.kinesis.readservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
